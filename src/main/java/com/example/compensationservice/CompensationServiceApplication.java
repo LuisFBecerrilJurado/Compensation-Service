f64e9d7b39e97f0aff7abe3275b05c3506f9302e
fran <l.francisco.bj@gmail.com>
2024-09-18 18:40:05 -0600

    Iniciando servicio de compensaciones

---
+++ src/main/java/com/example/compensationservice/CompensationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.compensationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompensationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompensationServiceApplication.class, args);
+    }
+
+}
