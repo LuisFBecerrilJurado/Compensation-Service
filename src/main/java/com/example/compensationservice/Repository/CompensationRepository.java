0050d3032d8aab9e05eedd5da1b46201f19ec6c5
fran <l.francisco.bj@gmail.com>
2024-09-23 15:41:40 -0600

    Change ID type in CompensationRepository to String
    Updated the CompensationRepository interface to use String as the ID type instead of Long. This change ensures consistency with other repositories that also use String IDs.

--- src/main/java/com/example/compensationservice/Repository/CompensationRepository.java
+++ src/main/java/com/example/compensationservice/Repository/CompensationRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CompensationRepository extends JpaRepository<Compensation, Long> {
+public interface CompensationRepository extends JpaRepository<Compensation, String> {
 
 }
