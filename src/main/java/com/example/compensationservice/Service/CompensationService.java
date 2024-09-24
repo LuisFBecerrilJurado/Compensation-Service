0ace9fb556808aab2350d57a0cb08ed56f3fbbd8
fran <l.francisco.bj@gmail.com>
2024-09-23 20:05:45 -0600

    Refactor CompensationService interface method signatures
    Renamed methods for better clarity and adjusted the saveCompensation return type. The getCompensationByUserId method is now named getCompensationByIdUser, and saveCompensation now returns void. These changes improve readability and consistency in the codebase.

--- src/main/java/com/example/compensationservice/Service/CompensationService.java
+++ src/main/java/com/example/compensationservice/Service/CompensationService.java
@@ -2,14 +2,13 @@ package com.example.compensationservice.Service;
 
 import com.example.compensationservice.Entities.Compensation;
 
-import java.time.LocalDate;
 import java.util.List;
 
 public interface CompensationService {
     List<Compensation> getAllCompensations();
     Compensation getCompensationById(String id);
-    List<Compensation> getCompensationByUserId(String userId);
-    Compensation saveCompensation(Compensation compensation);
+    List<Compensation> getCompensationByIdUser(String idUser);
+    void saveCompensation(Compensation compensation);
     void deleteCompensation(String id);
     void updateCompensation(Compensation compensation);
 }
