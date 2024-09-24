800b12f086a9f6fcaa41ed67df8ce56526319808
fran <l.francisco.bj@gmail.com>
2024-09-23 19:11:53 -0600

    Add CompensationController to manage compensation operations
    This commit introduces the CompensationController to handle HTTP requests related to compensation, including CRUD operations. It provides endpoints for creating, reading, updating, and deleting compensation records, along with specific methods to manage compensations by user ID.

---
+++ src/main/java/com/example/compensationservice/Controller/CompensationController.java
@@ -0,0 +1,72 @@
+package com.example.compensationservice.Controller;
+
+import com.example.compensationservice.Entities.Compensation;
+import com.example.compensationservice.Exceptions.CompensationException;
+import com.example.compensationservice.Service.CompensationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/compensations")
+public class CompensationController {
+    @Autowired
+    private CompensationService compensationService;
+
+    @GetMapping()
+    public ResponseEntity<List<Compensation>> getAllCompensations() {
+        return ResponseEntity.ok(compensationService.getAllCompensations());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Compensation> getCompensationById(@PathVariable String id) {
+        return ResponseEntity.ok(compensationService.getCompensationById(id));
+    }
+
+    @PostMapping("/newCompensation")
+    public ResponseEntity<String> saveCompensation(@RequestBody Compensation compensation) {
+        compensationService.saveCompensation(compensation);
+        return ResponseEntity.ok( "Compensation created successfully " );
+    }
+
+    public Compensation buildUpdateCompensation(Compensation existingCompensation, Compensation compensation) {
+        return Compensation.builder()
+                .idCompensation(existingCompensation.getIdCompensation())
+                .amount(compensation.getAmount())
+                .description(compensation.getDescription())
+                .salaryAdded(compensation.getSalaryAdded())
+                .type(compensation.getType())
+                .dateCompensation(compensation.getDateCompensation())
+                .build();
+    }
+
+    @PutMapping("/newCompensation/{id}")
+    public ResponseEntity<String> updateCompensation(@PathVariable String id, @RequestBody Compensation compensation) {
+        Optional<Compensation> compensationOptional = Optional.ofNullable(compensationService.getCompensationById(id));
+        if(compensationOptional.isPresent()){
+            Compensation existingCompensation = compensationOptional.get();
+            Compensation updatedCompensation =  buildUpdateCompensation(existingCompensation, compensation);
+            compensationService.updateCompensation(updatedCompensation);
+            return ResponseEntity.status(HttpStatus.OK).body(" User Updated Successfully");
+        }
+        throw new CompensationException("Compensation not found with id: " + id);
+    }
+
+    @GetMapping("/users/{idUser}")
+    public ResponseEntity<List<Compensation>> getCompensationByUserId(@PathVariable String idUser) {
+        return ResponseEntity.ok(compensationService.getCompensationByUserId(idUser));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteCompensation(@PathVariable String id) {
+        compensationService.deleteCompensation(id);
+        return ResponseEntity.ok("Compensation deleted successfully");
+    }
+
+}
