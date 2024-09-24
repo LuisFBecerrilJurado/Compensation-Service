5854dbb0ab22c1d38c17a8791a807e60c2c93708
fran <l.francisco.bj@gmail.com>
2024-09-23 19:12:36 -0600

    Add custom query methods to CompensationRepository
    Introduce `findByIdUser` to fetch compensations by user ID and `existsByDateCompensation` to check for compensations by date. This enhances the repository's capability to serve specific compensation-related queries.

--- src/main/java/com/example/compensationservice/Repository/CompensationRepository.java
+++ src/main/java/com/example/compensationservice/Repository/CompensationRepository.java
@@ -4,7 +4,10 @@ import com.example.compensationservice.Entities.Compensation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CompensationRepository extends JpaRepository<Compensation, String> {
-
+    List<Compensation> findByIdUser(String userId);
+    boolean existsByDateCompensation(String date);
 }
